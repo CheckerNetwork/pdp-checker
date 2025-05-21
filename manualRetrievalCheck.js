0e5131cd9a82f993d2c7063bc44e2f6a919fad43
Nikolas Haimerl <nhaimerl@Nikolass-MacBook-Pro.local>
2025-05-21 23:25:29 +0200

    remove manual testing

--- manualRetrievalCheck.js
+++
@@ -1,8 +0,0 @@
-import { checkRetrieval } from './lib/retrieval.js'
-const baseUrl = 'yablu.net'
-const pieceCid = 'baga6ea4seaqkzso6gijktpl22dxarxq25iynurceicxpst35yjrcp72uq3ziwpi';
-
-(async () => {
-  const result = await checkRetrieval(baseUrl, pieceCid)
-  console.log('CID match:', result)
-})()
