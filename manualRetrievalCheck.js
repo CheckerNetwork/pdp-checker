3c7fd257f23855b354b66bc2d50d13d61e6a3840
Srdjan S <stankovic.srdjo@gmail.com>
2025-05-21 14:45:47 +0200

    Use verifier contract with ethers

--- manualRetrievalCheck.js
+++ manualRetrievalCheck.js
@@ -3,7 +3,7 @@ import { checkRetrieval } from './retrieval/lib/retrieval.js'
 import { createVerifierContract } from './retrieval/lib/verifier.js'
 
 const baseUrl = 'yablu.net'
-const pieceCid = 'baga6ea4seaqkzso6gijktpl22dxarxq25iynurceicxpst35yjrcp72uq3ziwpi'
+const pieceCid = 'baga6ea4seaqkzso6gijktpl22dxarxq25iynurceicxpst35yjrcp72uq3ziwpi';
 
 (async () => {
   const verifier = createVerifierContract(VERIFIER_ADDRESS)
--- retrieval/lib/verifier.js
+++ retrieval/lib/verifier.js
@@ -1,19 +1,25 @@
+import { ethers } from 'ethers'
+import { rpcUrls, GLIF_TOKEN, WALLET_SEED } from './config.js';
+
+const abi = [
+  "function submitRetrievalResults(address service, string[] commps, bool[] retrievable)"
+];
+
 /*
 * @param {string} address
 */
 export const createVerifierContract = (address) => {
-  return {
-    /*
-    * @param {string} service
-    * @param {string[]} commps
-    * @param {boolean[]} retrievable
-    */
-    async submitRetrievalResults (service, commps, retrievable) {
-      // Mock implementation: just log the call
-      console.log('submitRetrievalResults called with:')
-      console.log('Service:', service)
-      console.log('CommPs:', commps)
-      console.log('Retrievable:', retrievable)
-    }
-  }
+  const provider = new ethers.FallbackProvider(rpcUrls.map(rpcUrl => {
+    const fetchRequest = new ethers.FetchRequest(rpcUrl)
+    fetchRequest.setHeader('Authorization', `Bearer ${GLIF_TOKEN}`)
+    return new ethers.JsonRpcProvider(fetchRequest)
+  }))
+
+  const signer = ethers.Wallet.fromPhrase(WALLET_SEED, provider)
+
+  return new ethers.Contract(
+    address,
+    abi,
+    signer
+  )
 }
