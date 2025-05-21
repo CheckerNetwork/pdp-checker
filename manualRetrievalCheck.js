cf554a15a8cf779de3c3c05a969d1b776ee502b8
Nikolas Haimerl <nhaimerl@Mac.telekom.ip>
2025-05-21 11:07:19 +0200

    add manual retrieval

---
+++ manualRetrievalCheck.js
@@ -0,0 +1,8 @@
+import { checkRetrieval } from './retrieval/lib/retrieval.js'
+const baseUrl = 'yablu.net'
+const pieceCid = 'baga6ea4seaqkzso6gijktpl22dxarxq25iynurceicxpst35yjrcp72uq3ziwpi';
+
+(async () => {
+  const result = await checkRetrieval(baseUrl, pieceCid)
+  console.log('CID match:', result)
+})()
